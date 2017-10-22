93e9717d1a22f4a90148b04a14fd6910f149bc7c
louvre2489 <louvre2489@gmail.com>
2017-10-22 16:16:42 +0900

    chapter9.Times We've Livin' In

--- src/main/java/money/Franc.java
+++ src/main/java/money/Franc.java
@@ -2,11 +2,11 @@ package money;
 
 public class Franc extends Money {
 
-  Franc(int amount) {
-    this.amount = amount;
+  Franc(int amount, String currency) {
+    super(amount, currency);
   }
 
   Money times(int multiplier) {
-    return new Franc(this.amount * multiplier);
+    return Money.franc(this.amount * multiplier);
   }
 }
